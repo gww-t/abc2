7277c6738d53acc23382d5c7e5a35995ec33c5dc
gww-t <3529537491@qq.com>
2021-03-14 01:19:37 +0800

    First commit

---
+++ 代码B/代码B.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+struct info
+{
+    int a;
+    int b;
+}unit[103];
+int cmd(info x, info y)
+{
+    return x.b < y.b;
+}
+int main()
+{
+    int n, i, j, k;
+    while (scanf("%d", &n) != EOF)
+    {
+        if (n == 0)
+            break;
+        for (i = 0; i < n; i++)
+        {
+            scanf("%d%d", &unit[i].a, &unit[i].b);
+        }
+        sort(unit, unit + n, cmd);
+        j = 0;
+        k = 0;
+        for (i = 1; i < n; i++)
+        {
+            if (unit[i].a >= unit[j].b)
+            {
+                k++;
+                j = i;
+            }
+        }
+        printf("%d\n", k + 1);
+    }
+    return 0;
+}
